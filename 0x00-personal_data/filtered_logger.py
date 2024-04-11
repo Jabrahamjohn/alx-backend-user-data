3e3fd83250f4d04afe8ee925ef344313e611f68c
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-11 22:58:39 +0300

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
-
-import logging, re
-from typing import List
-
-def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        """replaces the field with the redaction"""
-        message = re.sub(rf'{field}=.*?{separator}',
-                         """
-                         args:
-                         fields: {fields}
-                         redaction: {redaction}
-                         message: {message}
-                         separator: {separator}
-                         """
-                         f'{field}={redaction}{separator}', message)
-    return message
+import logging
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: list[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        filtered_values = map(self.filter_datum, [getattr(record, field, '') for field in self.fields])
+        record.msg = self.SEPARATOR.join(filtered_values)
+        return super().format(record)
+
+    def filter_datum(self, message: str) -> str:
+        return self.REDACTION
